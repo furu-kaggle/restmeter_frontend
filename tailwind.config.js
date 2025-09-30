fb50961bfb01aed31c4c2371bced9db98beac53c
古郡国彦 <kokoorikunihiko@MacBook-Air.local>
2025-09-30 18:45:22 +0900

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
+        heading: ['Zen Maru Gothic', 'sans-serif'],
+        body: ['Noto Sans JP', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
